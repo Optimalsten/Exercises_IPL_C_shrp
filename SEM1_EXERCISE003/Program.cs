65bdc9f712ab8b6e5fe6e15c0aaf59306cd94573
Optimalsten <soa@prg.spb.ru>
2023-09-02 20:14:22 +0300

    add in EXERCICIESES new project - SEMINAR 1 EXERCISE 003

---
+++ SEM1_EXERCISE003/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача № 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3   ->  Среда
+// 5   ->  Пятница
+
+Console.Write("Введите номер дня недели: ");
+string strNum = Console.ReadLine();
+int WeekDay = int.Parse(strNum);
+
+if (WeekDay == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+
+else if (WeekDay == 2)
+{
+    Console.WriteLine("Вторник");
+}
+
+else if (WeekDay == 3)
+{
+    Console.WriteLine("Среда");
+}
+
+else if (WeekDay == 4)
+{
+    Console.WriteLine("Четверг");
+}
+
+else if (WeekDay == 5)
+{
+    Console.WriteLine("Пятница");
+}
+
+else if (WeekDay == 6)
+{
+    Console.WriteLine("Суббота");
+}
+
+else if (WeekDay == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
+else
+{
+    Console.WriteLine("Вы ввели некорректный номер дня недели !");
+}
