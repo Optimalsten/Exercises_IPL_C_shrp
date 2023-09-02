c3d09cb94663eb0e7f4a18a0cc7e3f1ea5e8908a
Optimalsten <soa@prg.spb.ru>
2023-09-02 20:28:34 +0300

    feat: change programm text SEMINAR 1 EXERCISE 003

--- SEM1_EXERCISE003/Program.cs
+++ SEM1_EXERCISE003/Program.cs
@@ -3,40 +3,40 @@
 // 5   ->  Пятница
 
 Console.Write("Введите номер дня недели: ");
-string strNum = Console.ReadLine();
-int WeekDay = int.Parse(strNum);
+string WeekDay = Console.ReadLine();
+// int WeekDay = int.Parse(strNum);
 
-if (WeekDay == 1)
+if (WeekDay == "1")
 {
     Console.WriteLine("Понедельник");
 }
 
-else if (WeekDay == 2)
+else if (WeekDay == "2")
 {
     Console.WriteLine("Вторник");
 }
 
-else if (WeekDay == 3)
+else if (WeekDay == "3")
 {
     Console.WriteLine("Среда");
 }
 
-else if (WeekDay == 4)
+else if (WeekDay == "4")
 {
     Console.WriteLine("Четверг");
 }
 
-else if (WeekDay == 5)
+else if (WeekDay == "5")
 {
     Console.WriteLine("Пятница");
 }
 
-else if (WeekDay == 6)
+else if (WeekDay == "6")
 {
     Console.WriteLine("Суббота");
 }
 
-else if (WeekDay == 7)
+else if (WeekDay == "7")
 {
     Console.WriteLine("Воскресенье");
 }
